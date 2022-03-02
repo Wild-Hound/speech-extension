b426b47ea825f4db308c4835b81398ef5a289448
root <root@Vector.localdomain>
2022-03-02 16:35:54 +0600

    highlight fixed

--- src/Components/Utils/Functions/HighlightText.ts
+++ src/Components/Utils/Functions/HighlightText.ts
@@ -3,9 +3,24 @@ export function highlightText(textToHighlight: string, source: HTMLElement) {
   const pattern = new RegExp(modString, "gi");
 
   const temp = source.innerHTML.toString();
-  source.innerHTML = temp.replace(pattern, (match) => `<mark>${match}</mark>`);
+  const cleanedHtml = cleanSource(temp);
+  source.innerHTML = cleanedHtml.replace(
+    pattern,
+    (match) => `<mark>${match}</mark>`
+  );
 }
 
 function escapeRegExp(str: string) {
   return str.replace(/[\-\[\]\/\{\}\(\)\*\+\?\.\\\^\$\|]/g, "\\$&");
+}
+
+function cleanSource(str: string) {
+  const pattern1 = new RegExp(/<\/?mark>/g, "gi");
+  // const pattern2 = new RegExp(/</>/g, "gi");
+  // let cleanedString = str.replace("<mark>", "");
+  // cleanedString = cleanedString.replace("</mark>", "");
+
+  let cleanedString = str.replace(pattern1, (match) => "");
+  // cleanedString = str.replace(pattern1, (match) => ``);
+  return cleanedString;
 }
